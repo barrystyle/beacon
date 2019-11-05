45f10d3ce72fdc93e4c1e4072f90ab330ebabcc1
Aleš Kalan <SloRunner@users.noreply.github.com>
2019-11-05 18:34:48 +0100

    Change RPC default port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 11116;
     }
 };
 static CBaseMainParams mainParams;
