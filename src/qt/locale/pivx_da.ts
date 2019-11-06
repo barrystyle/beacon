c264fe065d7af14f3b7a4f8594f65435227f6bb5
Aleš Kalan <SloRunner@users.noreply.github.com>
2019-11-06 14:55:56 +0100

    Replace ports

--- src/qt/locale/pivx_da.ts
+++ src/qt/locale/pivx_da.ts
@@ -4912,8 +4912,8 @@ Vær tålmodig...</translation>
         <translation>Der skal angives en masternodeprivkey i konfigurationen. For mere information, se i dokumentationen.</translation>
     </message>
     <message>
-        <source>(51472 could be used only on mainnet)</source>
-        <translation>(51472 kunne kun bruges på hovednettet)</translation>
+        <source>(11115 could be used only on mainnet)</source>
+        <translation>(11115 kunne kun bruges på hovednettet)</translation>
     </message>
     <message>
         <source>(default: %s)</source>
@@ -4924,8 +4924,8 @@ Vær tålmodig...</translation>
         <translation>(default: 1)</translation>
     </message>
     <message>
-        <source>(must be 51472 for mainnet)</source>
-        <translation>(skal være 51472 for hovednettet)</translation>
+        <source>(must be 11115 for mainnet)</source>
+        <translation>(skal være 11115 for hovednettet)</translation>
     </message>
     <message>
         <source>Accept command line and JSON-RPC commands</source>
